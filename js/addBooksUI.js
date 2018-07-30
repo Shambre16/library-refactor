db365e036a1320b64ddc8b77638a9301b4770b99
ShambreSW <ssenawright@techtonicgroup.com>
2018-07-30 11:03:01 -0600

    Gets addBookUI up and running in the console, but still need to add cover field.  Gets showRandomBookUI up and running fully, but still need to format date.

--- js/addBooksUI.js
+++ js/addBooksUI.js
@@ -2,7 +2,7 @@
 class AddBooksUI extends Library {
   constructor(container) {
     super();
-    this._tempBookshelf = new Array();
+    this._queueArray = new Array();
     this.$container = container;
   }
   // Library.call(this);
@@ -17,23 +17,64 @@ class AddBooksUI extends Library {
   };
 
   _bindEvents() {
+    //this = my instance
     $('#addBookBtn').on('click', $.proxy(this._handleModalOpen, this));
-    $('#addToQueueBtn').on('click', $.proxy(this._handleQueueing, this));
-    $('#addToBookshelfBtn').on('click', $.proxy(this._handleAddToBookshelf, this));
+    this.$container.find('#addToQueueBtn').on('click', $.proxy(this._handleQueueing, this));
+    this.$container.find('#addToBookshelfBtn').on('click', $.proxy(this._handleAddToBookshelf, this));
+    this.$container.find('.clear-queue').on('click', $.proxy(this._handleClearQueue, this));
   };
 
   _handleModalOpen() {
     this.$container.modal('show');
   };
 
-  _handleQueueing(){
-alert("Queue it up!");
+  _handleQueueing(e){
+    e.preventDefault();
+    var serializedForm = this.$container.find('form').serializeArray();
+    // console.log(serializedForm);
+    var bookObject = new Object();
+    // console.log(bookObject);
+    var wasBookValid = true;
+
+    $.each(serializedForm, function(index, kvp){
+    if(kvp.value) {
+      bookObject[kvp.name] = kvp.value;
+    } else {
+      wasBookValid = false;
+      alert("Please enter value for: " + kvp.name);
+    }
+  });
+    // console.log("object create: " + bookObject);
+    // console.log(bookObject);
+
+  if(wasBookValid) {
+    this.$container.find('.queue-number').text(this._queueArray.push(bookObject));
   }
+  // console.log(this._queue);
+  return wasBookValid;
+};
+
 
   _handleAddToBookshelf() {
-alert("Yo shit's been added!");
-
+    if(this._queueArray.length) {
+    // console.log("first check");
+      if(this.addBooks(this._queueArray)) {
+        // console.log("was added");
+        this._handleClearQueue();
+    }
+  } else {
+    alert("Please queue at least one book.")
   }
+  return false;
+};
+
+  _handleClearQueue() {
+  this._queueArray = new Array();
+  this.$container.find('.queue-number').text(this._queueArray.length);
+  return false;
+};
+
+
 }
 
 $(function() {
--- js/showRandomBookUI.js
+++ js/showRandomBookUI.js
@@ -19,7 +19,7 @@ class ShowRandomBookUI extends Library {
     // var authors = this.getAuthors();
     // if (authors.length) {
       this.$container.modal('show');
-      this.$container.find('.modal-body').html(this._handleDisplayRandomBook());
+      this.$container.find('.modal-body').html(this._handleCreateRandoCard());
     //   this.$container.find('.modal-body').html(this._createUlOfAuthors(authors));
     // } else {
     //   alert('No books, and therefore no authors, exist in your library yet!');
@@ -28,15 +28,28 @@ class ShowRandomBookUI extends Library {
     // return false;
   };
 
-  _handleDisplayRandomBook(authors) {
-    var randomBook = this.getRandomBook();
-    var ul = document.createElement("ul");
-    for (var i = 0; i < authors.length; i++) {
-      var li = document.createElement("li");
-      $(li).text(authors[i]);
-      ul.append(li)
-    }
-    return ul;
+  _handleCreateRandoCard() {
+    let randomBookObject = this.getRandomBook();
+      $(".card-img-top").attr("src", randomBookObject.cover);
+      $(".card-title").text(randomBookObject.title);
+      $(".author-paragraph").text(randomBookObject.author);
+      $(".numberOfPages-paragraph").text(randomBookObject.numberOfPages);
+      $(".publishDate-paragraph").text(randomBookObject.publishDate);
+    // this.getRandomBookAjax(bookObject);
+    //   var randomBook = this.getRandomBook();
+    //   var cardDiv = document.createElement("div").addClass("card").attr("id", "randomBookModal");
+    //   var img = document.createElement("img").addClass("card-img-top").attr("src", "");
+    //   var cardBodyDiv = document.createElement("div").addClass("card-body");
+    //   var title = document.createElement("h5").addClass("card-title");
+    //   var details = document.createElement("p").addClass("card-text");
+    //
+    //   for (var i = 0; i < randomBook.length; i++) {
+    //     $(cardDiv).text(randomBook[i]);
+    //     $(img).text(randomBook[i]);
+    //     $(cardBodyDiv).text(randomBook[i]);
+    //     $(title).text(randomBook[i]);
+    //     $(details).text(randomBook[i]);
+    // }
   };
 
 }
